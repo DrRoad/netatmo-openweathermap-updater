af8d4e9059d3942750e8260fe275603f962abfbc
Juha Ristolainen <juha.ristolainen@iki.fi>
2017-11-05 18:10:34 +0100

    Store all modules and main station data to Table Storage

--- NetatmoOpenWeatherMapUpdater/Functions/StoreToTableStorage.cs
+++ NetatmoOpenWeatherMapUpdater/Functions/StoreToTableStorage.cs
@@ -9,30 +9,99 @@ namespace NetatmoOpenWeatherMapUpdater
     {
         [FunctionName("StoreToTableStorage")]
         public static void Run(
-            [QueueTrigger("%QUEUE_TABLESTORAGE%", Connection = "StorageConnectionString")]NetatmoStationData measurements,
+            [QueueTrigger("%QUEUE_TABLESTORAGE%", Connection = "StorageConnectionString")]NetatmoStationData stationData,
             [Table("%TABLE_MEASUREMENTS%", Connection = "StorageConnectionString")] ICollector<TableStorageMeasurement> tableStorage,
             TraceWriter log)
         {
-            var temperature = measurements.Body?.Devices?[0]?.Modules?[0]?.DashboardData?.Temperature;
-            var humidity = measurements.Body?.Devices?[0]?.Modules?[0]?.DashboardData?.Humidity;
-            var timestampOption = measurements.Body?.Devices?[0]?.Modules?[0]?.DashboardData?.TimeUtc;
-            if (!timestampOption.HasValue)
+
+            foreach (var device in stationData.Body?.Devices)
             {
-                log.Error("ERROR no timestamp.");
-                return;
+                log.Info($"Device {device.Id}");
+
+                foreach (var module in device.Modules)
+                {
+                    log.Info($"Module {module.Id}");
+                    var data = module.DashboardData;
+
+                    var moduleDataToStore = CreateModuleDto(device, module, data);
+                    tableStorage.Add(moduleDataToStore);
+                }
+
+                var deviceDataToStore = CreateDeviceDto(device);
+                tableStorage.Add(deviceDataToStore);
             }
+        }
 
-            DateTime timestamp = timestampOption.Value.FromUnixTimestampToDateTime();
-
-            var tableEntity = new TableStorageMeasurement()
+        private static TableStorageMeasurement CreateDeviceDto(Device device)
+        {
+            var data = device.DashboardData;
+            return new TableStorageMeasurement()
             {
-                PartitionKey = measurements.Body?.Devices?[0]?.Modules?[0]?.Id,
-                RowKey = $"{timestampOption.Value}",
-                Temperature = temperature.Value,
-                Humidity = humidity.Value
+                PartitionKey = device.Id,
+                RowKey = $"{data.TimeUtc}",
+                StatioName = device.StationName,
+                ModuleName = device.ModuleName,
+                ModuleType = device.Type,
+                UnixTimestamp = data.TimeUtc,
+                Temperature = data.Temperature,
+                TemperatureTrend = data.TempTrend,
+                MinTemperature = data.MinTemp,
+                MaxTemperature = data.MaxTemp,
+                MinTemperatureUnixTimestamp = data.DateMinTemp,
+                MaxTemperatureUnixTimestamp = data.DateMaxTemp,
+                Humidity = data.Humidity,
+                CO2 = data.CO2,
+                AbsolutePressure = data.AbsolutePressure,
+                Pressure = data.Pressure,
+                PressureTrend = data.PressureTrend,
+                Noise = data.Noise,
+                LastMessage = device.LastStatusStore,
+                LastSeen = device.LastStatusStore,
+                LastSetup = device.LastSetup,
+                LastUpgrade = device.LastUpgrade,
+                WifiStatus = device.WifiStatus,
+                Firmware = device.Firmware,
+                Longitude = device.Place?.Location?[1],
+                Latitude = device.Place?.Location?[0],
+                Altitude = device.Place?.Altitude,
+                City = device.Place?.City,
+                Country = device.Place?.Country,
+                Timezone = device.Place?.Timezone
             };
+        }
 
-            tableStorage.Add(tableEntity);
+        private static TableStorageMeasurement CreateModuleDto(Device device, Module module, FluffyDashboardData data)
+        {
+            return new TableStorageMeasurement()
+            {
+                PartitionKey = module.Id,
+                RowKey = $"{data.TimeUtc}",
+                StatioName = device.StationName,
+                ModuleName = module.ModuleName,
+                ModuleType = module.Type,
+                UnixTimestamp = data.TimeUtc,
+                Temperature = data.Temperature,
+                TemperatureTrend = data.TempTrend,
+                MinTemperature = data.MinTemp,
+                MaxTemperature = data.MaxTemp,
+                MinTemperatureUnixTimestamp = data.DateMinTemp,
+                MaxTemperatureUnixTimestamp = data.DateMaxTemp,
+                Humidity = data.Humidity,
+                CO2 = data.CO2,
+                LastMessage = module.LastMessage,
+                LastSeen = module.LastSeen,
+                LastSetup = module.LastSetup,
+                BatteryPercentage = module.BatteryPercent,
+                BatteryVp = module.BatteryVp,
+                RfStatus = module.RfStatus,
+                Firmware = module.Firmware,
+                Longitude = device.Place?.Location?[1],
+                Latitude = device.Place?.Location?[0],
+                Altitude = device.Place?.Altitude,
+                City = device.Place?.City,
+                Country = device.Place?.Country,
+                Timezone = device.Place?.Timezone
+            };
         }
     }
 }
--- NetatmoOpenWeatherMapUpdater/Models/TableStorageMeasurement.cs
+++ NetatmoOpenWeatherMapUpdater/Models/TableStorageMeasurement.cs
@@ -9,7 +9,36 @@ namespace NetatmoOpenWeatherMapUpdater
 {
     public class TableStorageMeasurement : TableEntity
     {
+        public string StatioName { get; set; }
+        public string ModuleName { get; set; }
+        public string ModuleType { get; set; }
+        public long LastMessage { get; set; }
+        public long LastSeen { get; set; }
+        public long LastSetup { get; set; }
+        public long LastUpgrade { get; set; }
+        public long UnixTimestamp { get; set; }
         public double Temperature { get; set; }
+        public string TemperatureTrend { get; set; }
+        public double MinTemperature { get; set; }
+        public long MinTemperatureUnixTimestamp { get; set; }
+        public double MaxTemperature { get; set; }
+        public long MaxTemperatureUnixTimestamp { get; set; }
         public long Humidity { get; set; }
+        public long? CO2 { get; set; }
+        public double? AbsolutePressure { get; set; }
+        public double? Pressure { get; set; }
+        public string PressureTrend { get; set; }
+        public long? Noise { get; set; }
+        public long BatteryPercentage { get; set; }
+        public long BatteryVp { get; set; }
+        public long RfStatus { get; set; }
+        public long WifiStatus { get; set; }
+        public long Firmware { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public long? Altitude { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Timezone { get; set; }
     }
 }
